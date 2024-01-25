d125b639ac418d6726b57547706b8ed51d3e8bf3
Derek Prieur <DPrieur123@gmail.com>
2024-01-25 17:56:29 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,55 @@
 import ProviderCard, { ProviderCardProps } from "@/components/ProviderCard";
 import { data } from "../constants/data";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Home() {
   return (
-    <main className="w-full justify-center items-center h-full">
-      <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 pt-16 px-2 gap-4">
-        {data.map((provider: any) => (
-          <ProviderCard key={provider._id} provider={provider} />
-        ))}
+    <main className="w-full justify-center items-center h-full pt-16 px-2">
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-end gap-3">
+          <Select>
+            <SelectTrigger className="w-fit">
+              <SelectValue placeholder="STAR RATING" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="apple">Apple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-fit">
+              <SelectValue placeholder="SERVICES OFFERED" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="apple">Apple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-fit">
+              <SelectValue placeholder="DISTANCE" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectItem value="apple">Apple</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-4">
+          {data.map((provider: any) => (
+            <ProviderCard key={provider._id} provider={provider} />
+          ))}
+        </div>
       </div>
     </main>
   );
