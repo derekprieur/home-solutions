86aec6a9adb7051fb7d245696a0fd54097bbd7a9
Derek Prieur <DPrieur123@gmail.com>
2024-01-26 10:12:36 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,19 +13,24 @@ import { useEffect, useState } from "react";
 import ServicesFilter from "@/components/ServicesFilter";
 import axios from "axios";
 import { data } from "@/constants/data";
+import { Circle, Loader2 } from "lucide-react";
 
 export default function Home() {
   const [selectedRating, setSelectedRating] = useState<any>(null);
   const [selectedServices, setSelectedServices] = useState<any>([]);
   const [filteredData, setFilteredData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const fetchData = async () => {
     try {
       const response = await axios("/api/data");
       setFilteredData(response.data);
       console.log(response.data, "response.data");
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -52,42 +57,48 @@ export default function Home() {
   return (
     <main className="w-full justify-center items-center h-full pt-16 px-2">
       <div className="flex flex-col gap-4">
-        <div className="flex justify-end gap-3">
-          <Select
-            onValueChange={(value: any) => setSelectedRating(Number(value))}
-          >
-            <SelectTrigger className="w-fit text-gray-600 font-semibold text-sm">
-              <SelectValue placeholder="STAR RATING" className="" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value={"all"}>All Ratings</SelectItem>
-                <SelectItem value={"5"}>5 Stars</SelectItem>
-                <SelectItem value={"4.5"}>4 Stars</SelectItem>
-                <SelectItem value={"4"}>4 Stars</SelectItem>
-                <SelectItem value={"3.5"}>3.5 Stars</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-          <ServicesFilter
-            data={filteredData}
-            setData={setFilteredData}
-            setSelectedServices={setSelectedServices}
-          />
-          <Select>
-            <SelectTrigger className="w-fit">
-              <SelectValue placeholder="DISTANCE" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="apple">Apple</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-        {filteredData.length > 0 ? (
+        {!isLoading && (
+          <div className="flex justify-end gap-3">
+            <Select
+              onValueChange={(value: any) => setSelectedRating(Number(value))}
+            >
+              <SelectTrigger className="w-fit text-gray-600 font-semibold text-sm">
+                <SelectValue placeholder="STAR RATING" className="" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value={"all"}>All Ratings</SelectItem>
+                  <SelectItem value={"5"}>5 Stars</SelectItem>
+                  <SelectItem value={"4.5"}>4 Stars</SelectItem>
+                  <SelectItem value={"4"}>4 Stars</SelectItem>
+                  <SelectItem value={"3.5"}>3.5 Stars</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+            <ServicesFilter
+              data={filteredData}
+              setData={setFilteredData}
+              setSelectedServices={setSelectedServices}
+            />
+            <Select>
+              <SelectTrigger className="w-fit">
+                <SelectValue placeholder="DISTANCE" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="apple">Apple</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        )}
+        {isLoading ? (
+          <div className="w-full h-full items-center flex justify-center mt-52">
+            <Loader2 className="animate-spin text-green-500 w-12 h-12" />
+          </div>
+        ) : filteredData.length > 0 ? (
           <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-4">
-            {filteredData.map((provider: any) => (
+            {filteredData.map((provider) => (
               <ProviderCard key={provider._id} provider={provider} />
             ))}
           </div>
